7909e62d46ac131c78c446509b878d45f943ed70
julian rintha <julrintha@gmail.com>
2021-12-12 19:50:36 -0500

    checkingwhynowork

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,15 +1,22 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Home</h1>
-<?php
-
-if (is_logged_in(true)) {
-    echo "Welcome home, " . get_username();
-    //comment this out if you don't want to see the session variables
+<div class="container-fluid">
+    <h1>Home</h1>
+    <?php
+    /*if (is_logged_in(true)) {
+    echo "Welcome, " . get_username();
+} else {
+    echo "You're not logged in";
+}*/
+    //shows session info
     //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-}
-?>
+    $duration = "week";
+    ?>
+    <?php require(__DIR__ . "/../../partials/score_table.php"); ?>
+</div>
 <?php
-require(__DIR__ . "/../../partials/flash.php");
+require(__DIR__ . "/../../partials/footer.php");
 ?>
+
+
